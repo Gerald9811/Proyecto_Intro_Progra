f276aaeff04693372ae14f37d5b2e2bac8246e4d
Zukko28 <kenneth_c.t@hotmail.com>
2026-03-26 19:36:21 -0600

    Update Gestion_Estudiantes.java
    Added the complete code

--- src/Estudiantes/Gestion_Estudiantes.java
+++ src/Estudiantes/Gestion_Estudiantes.java
@@ -4,13 +4,265 @@
  */
 package Estudiantes;
 
-/**
- *
- * @author user
- */
+import javax.swing.JOptionPane;
+
 public class Gestion_Estudiantes {
-    public static void Crear_Estudiante(){}
-    public static void Modificar_Estudiante(){}
-    public static void Eliminar_Estudiante(){}
-    
+
+    public static Info_Estudiantes[] estudiantes = new Info_Estudiantes[1];
+    public static int contador = 0;
+
+    public static void menuEstudiantes() {
+
+        int opcion = 0;
+
+        do {
+            String opciones[] = {
+                "Agregar estudiante",
+                "Mostrar estudiantes",
+                "Buscar estudiante",
+                "Editar estudiante",
+                "Eliminar estudiante",
+                "Matricular estudiante a curso",
+                "Retirar estudiante de curso",
+                "Buscar Curso",
+                "Salir"
+            };
+
+            opcion = JOptionPane.showOptionDialog(null, "Seleccione una opcion",
+                    "Gestión de Estudiantes",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null, opciones, opciones[0]);
+
+            switch (opcion) {
+
+                case 0:
+                    agregarEstudiante();
+                    break;
+
+                case 1:
+                    mostrarEstudiantes();
+                    break;
+
+                case 2:
+                    buscarEstudiante();
+                    break;
+
+                case 3:
+                    editarEstudiante();
+                    break;
+
+                case 4:
+                    eliminarEstudiante();
+                    break;
+                case 5:
+                    matricularCurso();
+                    break;
+                case 6:
+                    retirarCurso();
+                    break;    
+                case 7:
+                    buscarCurso();
+                    break;         
+
+            }
+
+        } while (opcion != 8);
+
+    }
+
+    public static void agregarEstudiante() {
+
+        if (contador < estudiantes.length) {
+
+            int id = contador + 1;
+
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre del estudiante:");
+            int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del estudiante:"));
+            String correo = JOptionPane.showInputDialog("Ingrese el correo del estudiante:");
+            int celular = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el teléfono del estudiante:"));
+            int cedula = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cédula del estudiante:"));
+
+            estudiantes[contador++] = new Info_Estudiantes(nombre, id, edad, correo,celular,cedula);
+
+            JOptionPane.showMessageDialog(null, "Estudiante agregado correctamente");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "No se pueden agregar más estudiantes");
+        }
+
+    }
+
+    public static void mostrarEstudiantes() {
+
+        if (contador == 0) {
+            JOptionPane.showMessageDialog(null, "No hay estudiantes registrados");
+            return;
+        }
+
+        String info = "Lista de estudiantes:\n";
+
+        for (int i = 0; i < contador; i++) {
+            info += estudiantes[i].toString();
+            info += "\n";
+            info += "----------------------\n";
+        }
+
+        JOptionPane.showMessageDialog(null, info);
+
+    }
+
+    public static void buscarEstudiante() {
+
+        int idBuscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del estudiante:"));
+
+        Info_Estudiantes encontrado = null;
+
+        for (int i = 0; i < contador; i++) {
+            if (estudiantes[i].getIdEstudiante() == idBuscar) {
+                encontrado = estudiantes[i];
+                break;
+            }
+        }
+
+        if (encontrado != null) {
+            JOptionPane.showMessageDialog(null, encontrado.toString());
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontró el estudiante");
+        }
+
+    }
+
+    public static void editarEstudiante() {
+
+        int idBuscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del estudiante:"));
+
+        Info_Estudiantes estudianteEditar = null;
+
+        for (int i = 0; i < contador; i++) {
+            if (estudiantes[i].getIdEstudiante() == idBuscar) {
+                estudianteEditar = estudiantes[i];
+                break;
+            }
+        }
+
+        if (estudianteEditar != null) {
+
+            String nombre = JOptionPane.showInputDialog("Nuevo nombre:");
+            estudianteEditar.setNombreEstudiante(nombre);
+
+            int edad = Integer.parseInt(JOptionPane.showInputDialog("Nueva edad:"));
+            estudianteEditar.setEdadEstudiante(edad);
+
+            String correo = JOptionPane.showInputDialog("Nuevo correo:");
+            estudianteEditar.setCorreoEstudiante(correo);
+
+            int celular =  Integer.parseInt(JOptionPane.showInputDialog("Nuevo teléfono:"));
+            estudianteEditar.setCelularEstudiante(celular);
+
+            int cedula =  Integer.parseInt(JOptionPane.showInputDialog("Nueva cédula:"));
+            estudianteEditar.setNumeroCedulaES(cedula);
+
+            JOptionPane.showMessageDialog(null, "Estudiante actualizado");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontró el estudiante");
+        }
+
+    }
+
+    public static void eliminarEstudiante() {
+
+        int indice = -1;
+
+        int idBuscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del estudiante:"));
+
+        for (int i = 0; i < contador; i++) {
+            if (estudiantes[i].getIdEstudiante() == idBuscar) {
+                indice = i;
+                break;
+            }
+        }
+
+        if (indice == -1) {
+            JOptionPane.showMessageDialog(null, "No se encontró el estudiante");
+            return;
+        }
+
+        for (int i = indice; i < contador - 1; i++) {
+            estudiantes[i] = estudiantes[i + 1];
+        }
+
+        estudiantes[contador - 1] = null;
+        contador--;
+
+        JOptionPane.showMessageDialog(null, "Estudiante eliminado correctamente");
+
+    }
+
+ public static void matricularCurso() {
+
+    int idBuscar = Integer.parseInt(JOptionPane.showInputDialog("Digite el ID del estudiante:"));
+    Info_Estudiantes estudiante = null;
+
+    for (int i = 0; i < contador; i++) {
+        if (estudiantes[i].getIdEstudiante() == idBuscar) {
+            estudiante = estudiantes[i];
+            break;
+        }
+    }
+
+    if (estudiante != null) {
+
+        String curso = JOptionPane.showInputDialog("Digite el nombre del curso a matricular:");
+
+        JOptionPane.showMessageDialog(null, "Estudiante matriculado en el curso: " + curso);
+
+    } else {
+        JOptionPane.showMessageDialog(null, "No se encontró el estudiante");
+    }
+}   
+ 
+public static void retirarCurso() {
+
+    int idBuscar = Integer.parseInt(JOptionPane.showInputDialog("Digite el ID del estudiante:"));
+    Info_Estudiantes estudiante = null;
+
+    for (int i = 0; i < contador; i++) {
+        if (estudiantes[i].getIdEstudiante() == idBuscar) {
+            estudiante = estudiantes[i];
+            break;
+        }
+    }
+
+    if (estudiante != null) {
+
+        String curso = JOptionPane.showInputDialog("Digite el nombre del curso a retirar:");
+
+        JOptionPane.showMessageDialog(null, "Estudiante retirado del curso: " + curso);
+
+    } else {
+        JOptionPane.showMessageDialog(null, "No se encontró el estudiante");
+    }
+}    
+
+public static void buscarCurso() {
+
+    String cursoBuscar = JOptionPane.showInputDialog("Digite el nombre del curso:");
+
+    boolean encontrado = false;
+
+    for (int i = 0; i < contador; i++) {
+
+        if (estudiantes[i].toString().contains(cursoBuscar)) {
+            JOptionPane.showMessageDialog(null, estudiantes[i].toString());
+            encontrado = true;
+        }
+    }
+
+    if (!encontrado) {
+        JOptionPane.showMessageDialog(null, "No se encontró el curso");
+    }
+}
+
 }
